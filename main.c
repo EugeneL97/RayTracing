f135778172044167ed0524e96cf47441e3ba1c0c
eugenel97 <eugenejr14@gmail.com>
2025-01-20 07:55:21 -0800

    Implemented circle drawing and moving circles

--- main.c
+++ main.c
@@ -4,7 +4,29 @@
 #define WIDTH 900
 #define HEIGHT 600
 #define COLOR_WHITE 0xffffff
+struct Circle {
+    double x;
+    double y;
+    double radius;
+};
 
+
+void FillCircle(SDL_Surface* surface, struct Circle circle, Uint32 color)
+{
+    double radius_squared = pow(circle.radius, 2);
+    for (double x = circle.x - circle.radius; x <= circle.x + circle.radius; ++x)
+    {
+        for (double y = circle.y - circle.radius; y <= circle.y + circle.radius; ++y)
+        {
+            double distance_squared = pow(x-circle.x, 2) + pow(y - circle.y, 2);
+            if (distance_squared < radius_squared)
+            {
+                SDL_Rect pixel = (SDL_Rect){x,y,1,1};
+                SDL_FillRect(surface, &pixel, color);
+            }
+        }
+    }
+}
 int main(int argc, char* argv[]) {
 
     SDL_Init(SDL_INIT_VIDEO);
@@ -13,6 +35,26 @@ int main(int argc, char* argv[]) {
     SDL_Surface* surface = SDL_GetWindowSurface(window);
     SDL_Rect rect = (SDL_Rect){200,200,200,200};
     SDL_FillRect(surface, &rect, COLOR_WHITE);
+    struct Circle circle = {200, 200, 80};
+    struct Circle circle_shadow = {650, 300, 140};
+    int simulation_running = 1;
+    SDL_Event event;
+    while (simulation_running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                simulation_running = 0;
+            }
+            if (event.type == SDL_MOUSEMOTION && event.motion.state != 0)
+            {
+                circle.x = event.motion.x;
+                circle.y = event.motion.y;
+            }
+        }
+        FillCircle(surface, circle, COLOR_WHITE);
+        FillCircle(surface, circle_shadow, COLOR_WHITE);
 
     SDL_UpdateWindowSurface(window);
 
